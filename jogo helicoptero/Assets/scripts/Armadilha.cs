090e023fbdf7a32a68d584229b0db4018327751b
Tamires Lima da Silva <tami7703@gmail.com>
2016-03-04 03:01:40 -0300

    A armadilha não está sendo acionada. Verificar.

--- jogo helicoptero/Assets/scripts/Armadilha.cs
+++ jogo helicoptero/Assets/scripts/Armadilha.cs
@@ -4,20 +4,50 @@ using System.Collections;
 public class Armadilha : MonoBehaviour {
 
 	public GameObject Prefab;
+	private bool ativo;
+	public Transform SpawnPoint;
+
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+
+
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
+		
 
-	void OnTriggerEnter2D (Collider2D col){
-		if (col.gameObject.tag == "Player") {
-			GameObject tempPrefab = Instantiate (Prefab) as GameObject;	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "player")
+		if (!ativo) //if (!ativo) e a mesma coisa que if (ativo == false)
+
+		if (col.gameObject.tag == "Player") 
+			
+			{
+			GameObject tempPrefab = Instantiate (Prefab) as GameObject; 
+			tempPrefab.transform.position = SpawnPoint.position;
+				ativo = true;
+
+			}
+		}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+
+		if (col.tag == "player") 
+		{
+			ativo = false;
 		}
 	}
+
 }
+
+
+
